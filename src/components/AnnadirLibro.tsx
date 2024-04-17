64174038a6ac228ba23fb66e4466694a29e0d3bc
a19sotean <a19sotean@iesgrancapitan.org>
2024-04-17 20:09:24 +0200

    Modificación estilos

--- src/components/AnnadirLibro.tsx
+++ src/components/AnnadirLibro.tsx
@@ -6,7 +6,7 @@ export default function AnnadirLibro(props: { libro: WithId<LibroModel> }) {
         <div className="flex flex-col md:flex-row justify-center p-5 w-1/3">  
             <div className="flex-none md:ml-5 border-l-2 border-zinc-900 pl-5">
                 <p className="text-4xl font-bold">{props.libro.price.toString()}€</p>
-                <button className="mt-2 px-4 py-2 bg-zinc-900 text-zinc-300 text-lg">
+                <button className="mt-2 px-4 py-2 rounded bg-indigo-800 hover:bg-indigo-900 text-zinc-300 text-lg transition duration-300">
                     Añadir a la cesta
                 </button>
                 <p className="mt-2">Recíbelo mañana</p>
--- src/components/Novedades.tsx
+++ src/components/Novedades.tsx
@@ -5,15 +5,15 @@ const Novedades = ({ libro }: { libro: WithId<LibroModel> }) => {
     if (!libro) return <div className="flex justify-center items-center">No hay novedades disponibles.</div>;
 
     return (
-        <div className="flex justify-center items-center h-50vh">
-            <div className="bg-teal-300 p-10 w-full md:w-1/2 shadow-lg rounded-lg overflow-hidden">
+        <div className="flex justify-center items-center h-50vh p-10">
+            <div className="bg-zinc-900 text-zinc-200 p-10 w-full md:w-1/2 shadow-lg rounded-lg overflow-hidden">
                 <div className="flex flex-col md:flex-row justify-between items-center">
-                    <div className="text-zinc-900 mb-4 md:mb-0">
-                        <h1 className="text-5xl text-teal-800 font-bold mb-4 text-center">Novedades!!</h1>
+                    <div className="mb-4 md:mb-0">
+                        <h1 className="text-5xl text-indigo-300 font-bold mb-4 text-center">Novedades!!</h1>
                         <h2 className="text-3xl font-bold mb-2">{libro.name}</h2>
                         <p className="text-xl mb-4">{libro.opinion}</p>
-                        <p className="text-zinc-600">{libro.author}</p>
-                        <button className="mt-4 bg-teal-600 text-white py-2 px-4 rounded hover:bg-teal-700 transition duration-300">Comprar ahora →</button>
+                        <p className="text-zinc-400">{libro.author}</p>
+                        <button className="mt-4 bg-indigo-800 text-white py-2 px-4 rounded hover:bg-indigo-900 transition duration-300">Comprar ahora →</button>
                     </div>
                     <div className="h-auto drop-shadow-2xl relative">
                         <div className="clip-trapecio">
