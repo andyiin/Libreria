5034140a24a605fc128d59c9a583d1160694a627
casmurdor <a19camoan@users.noreply.github.com>
2024-04-19 00:00:07 +0200

    Añadidos estilos al login y register. Arreglado middleware

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,15 +1,46 @@
+import Link from "next/link";
+
 export default function Login(props: { handleLogin: (formData: FormData) => void }) {
     return (
-        <form action={props.handleLogin}>
-            <div>
-                <label htmlFor="email">Correo</label>
-                <input className="text-black" type="email" id="email" name="email" required />
-            </div>
-            <div>
-                <label htmlFor="password">Contraseña</label>
-                <input className="text-black" type="password" id="password" name="password" required />
-            </div>
-            <button type="submit">Iniciar sesión</button>
-        </form>
+        <div className="flex items-center justify-center min-h-screen bg-zinc-300">
+            <form className="bg-white shadow-md rounded-xl px-8 pt-6 pb-8 mb-4 w-full max-w-md" action={props.handleLogin}>
+                <h2 className="text-2xl font-bold mb-6 text-center text-indigo-800">Iniciar sesión</h2>
+                <div className="mb-4">
+                    <label className="block text-indigo-800 text-sm font-bold mb-2" htmlFor="email">
+                        Correo
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-indigo-800 leading-tight focus:outline-none focus:shadow-outline"
+                        type="email"
+                        id="email"
+                        name="email"
+                        required
+                    />
+                </div>
+                <div className="mb-6">
+                    <label className="block text-indigo-800 text-sm font-bold mb-2" htmlFor="password">
+                        Contraseña
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-indigo-800 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        type="password"
+                        id="password"
+                        name="password"
+                        required
+                    />
+                </div>
+                <div className="flex items-center justify-between">
+                    <button
+                        className="bg-indigo-800 hover:bg-indigo-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-300"
+                        type="submit"
+                    >
+                        Iniciar sesión
+                    </button>
+                    <Link href="/reset-password" className="inline-block align-baseline font-bold text-sm text-indigo-800 hover:text-indigo-900">
+                        ¿Has olvidado la contraseña?
+                    </Link>
+                </div>
+            </form>
+        </div>
     );
 };
