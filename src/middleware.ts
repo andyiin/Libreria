97e4846476c5fa830c1179f38b049143b73914c4
casmurdor <a19camoan@users.noreply.github.com>
2024-06-11 13:07:25 +0200

    Middleware actualizado

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,18 +5,21 @@ import { retrieve } from "@/lib/session"
 export async function middleware(request: NextRequest) {
     const user = await retrieve("user");
 
-    const protectedRoutes = ["/dashboard", "/logout"];
+    const protectedRoutes = ["/dashboard", "/logout", "/profile", "/create-book"];
     const publicRoutes = ["/login", "/register"];
 
-    if (protectedRoutes.includes(request.nextUrl.pathname) && !user)
-        return NextResponse.redirect(new URL("/", request.url))
+    if (protectedRoutes.some(route => request.nextUrl.pathname.startsWith(route)) && !user)
+        return NextResponse.redirect(new URL("/", request.url));
 
     if (publicRoutes.includes(request.nextUrl.pathname) && user)
-        return NextResponse.redirect(new URL("/", request.url))
+        return NextResponse.redirect(new URL("/", request.url));
+
+    if (protectedRoutes.some(route => request.nextUrl.pathname.startsWith(route)) && user?.rol !== "admin")
+        return NextResponse.redirect(new URL("/", request.url));
 
     return NextResponse.next()
 };
 
 export const config = {
-    matcher: ["/login", "/register", "/dashboard", "/logout"]
+    matcher: ["/login", "/register", "/dashboard", "/logout", "/profile/:path*", "/create-book"]
 };
